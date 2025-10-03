d262651efcec0ac8f4c0ed5f56e79e2b57b6e9eb
Saurabh209 <saurabhhh2613@gmail.com>
2025-10-03 23:04:55 +0530

    working on responsiveness

--- assignment/src/YourResume.jsx
+++ assignment/src/YourResume.jsx
@@ -3,6 +3,8 @@ import './YourResume.scss'
 
 import Girl from '../public/Girl.svg'
 import underline from '../public/underline.svg'
+import Star from '../public/star.svg'
+import Hat from '../public/hat.svg'
 
 const YourResume = () => {
     return (
@@ -22,17 +24,44 @@ const YourResume = () => {
                         <p>If your resume isn’t getting responses, it’s time for an upgrade. Get an ATS-optimized resume crafted by HR experts to help you land more interviews. Our resumes are designed to get your foot in the door and place your name at the top of the shortlist.</p>
                     </div>
                     <div className='yourResume-button-container'>
-                        <button>
+                        <button className='resume-package'>
                             Resume Package
                         </button>
 
-                        <button>
+                        <button className='contact-us'>
                             Contact Us
                         </button>
                     </div>
                 </div>
                 <div className='inner-container-image'>
-                    <img src={Girl} alt="girl" />
+                    <img src={Girl} alt="girl">
+                    </img>
+                    <div className='rating'>
+                        <div className='rating-number'>
+                            <p>4.9</p>
+                        </div>
+                        <div className='rating-text'>
+                            <div className='rating-text-star'>
+                                <img src={Star} alt="star" />
+                                <img src={Star} alt="star" />
+                                <img src={Star} alt="star" />
+                                <img src={Star} alt="star" />
+                                <img src={Star} alt="star" />
+                            </div>
+                            <div className='rating-text-text'><p>Instructor Rating</p></div>
+                        </div>
+                    </div>
+                    <div className='resume-created'>
+                        <div className='resume-created-logo'>
+                            <img src={Hat} alt="star" />
+                        </div>
+                        <div className='resume-creted-other-container '>
+                            <div className=' resume-created-number'>
+                                <p>260+</p>
+                            </div>
+                            <div className='resume-created-text '><p>Online Resume Created</p></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
